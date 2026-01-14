dde18df93ea5a3bc44c3b9dc78e7a833bb211ec6
Alvin Dong <alvindong9913@gmail.com>
2026-01-14 17:05:52 -0600

    gitignore, metadata json

--- AnimationCards.lua
+++ AnimationCards.lua
@@ -1,20 +1,10 @@
---- STEAMODDED HEADER
---- MOD_NAME: AnimationCards
---- MOD_ID: AnimationCards
---- MOD_AUTHOR: [Hoja]
---- MOD_DESCRIPTION: Add animation to cards.
---- BADGE_COLOUR: 69FA61
---- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-1404g]
---- VERSION: 1.0
-
 function debugMessage(message)
-    sendDebugMessage('[AnimationCards] ' .. message)
+	sendDebugMessage("[AnimationCards] " .. message)
 end
 debugMessage("Launching")
 
 G.F_NO_ACHIEVEMENTS = false
 local AnimationCards = SMODS.current_mod
-
 
 --- Texturas ---
 SMODS.load_file("textures/planets.lua")()
@@ -25,20 +15,24 @@ SMODS.load_file("textures/cards.lua")()
 -- ===========================
 Aura = Aura or {}
 
-function Aura.update_frame(dt, key, center_obj, anim_table)
-    local anim = anim_table[key]
-    if not (anim and center_obj) then return end
+if not Aura.update_frame then
+	function Aura.update_frame(dt, key, center_obj, anim_table)
+		local anim = anim_table[key]
+		if not (anim and center_obj) then
+			return
+		end
 
-    anim.t = (anim.t or 0) + dt
-    anim.frame = anim.frame or 0
-    local fps = anim.fps or 12 -- velocidad base
+		anim.t = (anim.t or 0) + dt
+		anim.frame = anim.frame or 0
+		local fps = anim.fps or 12 -- velocidad base
 
-    if anim.t >= 1 / fps then
-        anim.t = anim.t - (1 / fps)
-        anim.frame = (anim.frame + 1) % anim.frames
+		if anim.t >= 1 / fps then
+			anim.t = anim.t - (1 / fps)
+			anim.frame = (anim.frame + 1) % anim.frames
 
-        local fpr = anim.frames_per_row or anim.frames
-        center_obj.pos.x = anim.frame % fpr
-        center_obj.pos.y = math.floor(anim.frame / fpr)
-    end
+			local fpr = anim.frames_per_row or anim.frames
+			center_obj.pos.x = anim.frame % fpr
+			center_obj.pos.y = math.floor(anim.frame / fpr)
+		end
+	end
 end
